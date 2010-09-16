4eea4f65ef0b37e02891303327a7a26e1df00e54
Jiri Mares <jiramares@gmail.com>
2010-09-16 20:37:14 +0200

    Total refactoring with objects, added average quality.

--- ppm/playerQualities.js
+++ ppm/playerQualities.js
@@ -1,50 +1,91 @@
-var wing = [[2, 3, 5, 6], [100.0, 80.0, 58.0, 53.0]];
-var center = [[2, 3, 4, 5], [100.0, 65.0, 53.0, 53.0]];
-var defender = [[1, 4, 5, 6], [100.0, 58.0, 58.0, 53.0]];
-var goalkeeper = [[0, 4, 5], [100.0, 53.0, 53.0]];
-var values = [];
-var qualities = [];
-var experience = 0.0;
-var combination = 0.0;
+function Player() {
+	this.values = [];
+	this.qualities = [];
+	this.experience = 0.0;
+	this.combination = 0.0;
 
-function countQuality(type, qualities, size) {
-	var index = 0;
-	var quotient = 0;
-	var denominator = 0;
-	while (index < size) {
-		quotient += type[1][index];
-		denominator += type[1][index] / qualities[type[0][index]];
-		index++;
+	this.qualityAs = function(type) {
+		var index = 0;
+		var quotient = 0.0;
+		var denominator = 0.0;
+		while (index < 7) {
+			quotient += type.prefs[index];
+			denominator += type.prefs[index] / this.qualities[index];
+			index++;	
+		}
+		return parseInt(quotient / denominator);
+	};
+
+	this.valueAs = function(type) {
+		var index = 0;
+		var value = 0.0;
+		while (index < 7) {
+			value += type.prefs[index] * this.values[index] / 100;
+			index++;
+		}
+		return parseInt(value * (1 + 0.2 * this.experience / 100 + 0.2 * this.combination / 100));
 	}
-	return parseInt(quotient / denominator);
-}
+	
+	this.avgQuality = function() {
+		var index = 0;
+		var avg = 0.0;
+		while(index < this.qualities.length) {
+			avg += this.qualities[index++];
+		}
+		return parseInt(avg / this.qualities.length);
+	};
+};
 
-function countValue(type, values, size) {
-	var index = 0;
-	var value = 0;
-	while (index < size) {
-		value += type[1][index] * values[type[0][index]] / 100;
-		index++;
-	}
-	return parseInt(value * (1 + 0.2 * experience / 100 + 0.2 * combination / 100));
-}
+function PlayerType(name, prefs) {
+	this.name = name;
+	this.prefs = prefs;
+};
 
-$("table[cellspacing='4'] tr td[valign='top'] table.table_page").has("tr th:contains('Týmové informace')").find("tr:nth-child(4) td:nth-child(2)").each(function(index) {
-	if (index == 0) combination = parseFloat($(this).text().split('%'));
-});
-$("table[cellspacing='4'] tr td[valign='top'] table.table_page").has("tr th:contains('Atributy')").find("tr:nth-child(3) td").each(function(index) {
-	if (index < 7) {
-		var tmp = $(this).text().split(' ');
-		values[index] = parseFloat(tmp[0]);
-		qualities[index] = parseFloat(tmp[1]);
-	} else {
-		experience = parseFloat($(this).text());
-	}
-});
+function PlayerPage() {
+	this.parsePlayer = function(player) {
+		$("table[cellspacing='4'] tr td[valign='top'] table.table_page").has("tr th:contains('Týmové informace')").find("tr:nth-child(4) td:nth-child(2)").each(function(index) {
+				if (index == 0) player.combination = parseFloat($(this).text().split('%'));
+		});
+		$("table[cellspacing='4'] tr td[valign='top'] table.table_page").has("tr th:contains('Atributy')").find("tr:nth-child(3) td").each(function(index) {
+			if (index < 7) {
+				var tmp = $(this).text().split(' ');
+				player.values[index] = parseFloat(tmp[0]);
+				player.qualities[index] = parseFloat(tmp[1]);
+			} else {
+				player.experience = parseFloat($(this).text());
+			}
+		});
+	};
+	
+	this.outputQualityAsType = function(player, type) {
+		return "<tr><td>" + type.name + "</td><td>" + player.valueAs(type) + "<span class='space'> </span>" 
+			+ "<span class='kva2'><font color='gray'>" + player.qualityAs(type) + "</font></td></tr>";
+	};
+	
+	this.outputAvgQuality = function(player) {
+		return "<tr><td>Průměrná kvalita</td><td>" + player.avgQuality() + "</td></tr>";
+	};
+	
+	this.outputQualities = function(player, types) {
+		var qualities = this.outputAvgQuality(player);
+		var index = 0;
+		while (index < types.length) {
+			qualities += this.outputQualityAsType(player, types[index++]);
+		}
+		qualities = "<table class='table_page3' cellspacing='0' cellpadding='0'><tr><th colspan='2' align='center'>Kvalita hráče</th></tr>" + qualities + "</table>";
+		$("table[cellspacing='4'] tr td[valign='top'] table.table_page3").after(qualities);
+	};
+};
 
-var h = "<table class='table_page3' cellspacing='0' cellpadding='0'><tr><th colspan='2' align='center'>Kvalita hráče</th></tr>";
-var g = "<tr><td>Brankář</td><td>" + countValue(goalkeeper, values, 3) + "<span class='space'> </span><span class='kva2'><font color='gray'>" + countQuality(goalkeeper, qualities, 3) + "</font></td></tr>";
-var d = "<tr><td>Obránce</td><td>" + countValue(defender, values, 4) + "<span class='space'> </span><span class='kva2'><font color='gray'>" + countQuality(defender, qualities, 4) + "</font></td></tr>";
-var w = "<tr><td>Křídelník</td><td>" + countValue(wing, values, 4) + "<span class='space'> </span><span class='kva2'><font color='gray'>" + countQuality(wing, qualities, 4) + "</font></td></tr>";
-var c = "<tr><td>Centr</td><td>" + countValue(center, values, 4) + "<span class='space'> </span><span class='kva2'><font color='gray'>" + countQuality(center, qualities, 4) + "</font></td></tr>";
-$("table[cellspacing='4'] tr td[valign='top'] table.table_page3").after(h + g + d + w + c + "</table>");
+var types = [
+	new PlayerType("Křídelník", [0.0, 0.0, 100.0, 80.0, 0.0, 58.0, 53.0] ),
+	new PlayerType("Center", [0.0, 0.0, 100.0, 65.0, 53.0, 53.0, 0.0] ),
+	new PlayerType("Obránce", [0.0, 100.0, 0.0, 0.0, 58.0, 58.0, 53.0] ),
+	new PlayerType("Brankář", [100.0, 0.0, 0.0, 0.0, 53.0, 53.0, 0.0] )];
+
+var page = new PlayerPage();
+var player = new Player();
+page.parsePlayer(player);
+page.outputQualities(player, types);
+
+
