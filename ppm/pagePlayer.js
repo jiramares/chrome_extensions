255038648b6dbca68549d537ab482f7677cda8c5
Jiri Mares <jiramares@gmail.com>
2010-09-19 00:04:57 +0200

    Another refactoring, to be able to show additional data on another pages

--- ppm/pagePlayer.js
+++ ppm/pagePlayer.js
@@ -1,46 +1,3 @@
-function Player() {
-	this.values = [];
-	this.qualities = [];
-	this.experience = 0.0;
-	this.combination = 0.0;
-
-	this.qualityAs = function(type) {
-		var index = 0;
-		var quotient = 0.0;
-		var denominator = 0.0;
-		while (index < 7) {
-			quotient += type.prefs[index];
-			denominator += type.prefs[index] / this.qualities[index];
-			index++;	
-		}
-		return parseInt(quotient / denominator);
-	};
-
-	this.valueAs = function(type) {
-		var index = 0;
-		var value = 0.0;
-		while (index < 7) {
-			value += type.prefs[index] * this.values[index] / 100;
-			index++;
-		}
-		return parseInt(value * (1 + 0.2 * this.experience / 100 + 0.2 * this.combination / 100));
-	}
-	
-	this.avgQuality = function() {
-		var index = 0;
-		var avg = 0.0;
-		while(index < this.qualities.length) {
-			avg += this.qualities[index++];
-		}
-		return parseInt(avg / this.qualities.length);
-	};
-};
-
-function PlayerType(name, prefs) {
-	this.name = name;
-	this.prefs = prefs;
-};
-
 function PlayerPage() {
 	this.parsePlayer = function(player) {
 		$("table[cellspacing='4'] tr td[valign='top'] table.table_page").has("tr th:contains('Týmové informace')").find("tr:nth-child(4) td:nth-child(2)").each(function(index) {
@@ -68,20 +25,13 @@ function PlayerPage() {
 	
 	this.outputQualities = function(player, types) {
 		var qualities = this.outputAvgQuality(player);
-		var index = 0;
-		while (index < types.length) {
-			qualities += this.outputQualityAsType(player, types[index++]);
+		for (var index = 0; index < types.length; index++) {
+			qualities += this.outputQualityAsType(player, types[index]);
 		}
 		qualities = "<table class='table_page3' cellspacing='0' cellpadding='0'><tr><th colspan='2' align='center'>Kvalita hráče</th></tr>" + qualities + "</table>";
 		$("table[cellspacing='4'] tr td[valign='top'] table.table_page3").after(qualities);
 	};
 };
-
-var types = [
-	new PlayerType("Křídelník", [0.0, 0.0, 100.0, 80.0, 0.0, 58.0, 53.0] ),
-	new PlayerType("Center", [0.0, 0.0, 100.0, 65.0, 53.0, 53.0, 0.0] ),
-	new PlayerType("Obránce", [0.0, 100.0, 0.0, 0.0, 58.0, 58.0, 53.0] ),
-	new PlayerType("Brankář", [100.0, 0.0, 0.0, 0.0, 53.0, 53.0, 0.0] )];
 
 var page = new PlayerPage();
 var player = new Player();
---
+++ ppm/types.js
@@ -0,0 +1,51 @@
+function Player() {
+	this.values = [];
+	this.qualities = [];
+	this.experience = 0.0;
+	this.combination = 0.0;
+
+	this.qualityAs = function(type) {
+		var quotient = 0.0;
+		var denominator = 0.0;
+		for (var index = 0; index < 7; index++) {
+			quotient += type.prefs[index];
+			denominator += type.prefs[index] / this.qualities[index];
+		}
+		return parseInt(quotient / denominator);
+	};
+
+	this.valueAs = function(type) {
+		var value = 0.0;
+		for (var index = 0; index < 7; index++) {
+			value += type.prefs[index] * this.values[index] / type.maxPref;
+		}
+		return parseInt(value * (1 + 0.2 * this.experience / 100 + 0.2 * this.combination / 100));
+	}
+	
+	this.avgQuality = function() {
+		var sum = 0.0;
+		for (var index = 0; index < this.qualities.length; index++) {
+			sum += this.qualities[index];
+		}
+		return parseInt(sum / this.qualities.length);
+	};
+};
+
+function PlayerType(name, prefs) {
+	this.name = name;
+	this.prefs = prefs;
+	this.maxPref = 0.0;
+	
+	for (var index = 0; index < prefs.length; index++) {
+		if (prefs[index] > this.maxPref)
+			this.maxPref = prefs[index];
+	};
+};
+
+var types = [
+	new PlayerType("Křídelník", [0.0, 0.0, 100.0, 80.0, 0.0, 58.0, 53.0] ),
+	new PlayerType("Center", [0.0, 0.0, 100.0, 65.0, 53.0, 53.0, 0.0] ),
+	new PlayerType("Obránce", [0.0, 100.0, 0.0, 0.0, 58.0, 58.0, 53.0] ),
+	new PlayerType("Brankář", [100.0, 0.0, 0.0, 0.0, 53.0, 53.0, 0.0] )];
+
+
